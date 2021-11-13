97b13c21e3e08a20d4a14c29e3c90dc86c62cfa6
seolcu <gyuwon112@gmail.com>
2021-11-13 17:03:58 +0900

    To the Big Dipper, Gothic A1

--- pages/index.js
+++ pages/index.js
@@ -17,33 +17,29 @@ const Home = () => {
       </Head>
       <HeaderComponent />
       <div className={styles.mainOverlay}>
-        <div>
-          <h1>제 27회 한빛제</h1>
-          <Link href="/product">
-            <a>
-              <h2>한빛마켓</h2>
-            </a>
-          </Link>
-          <Link href="/vieworder">
-            <a>
-              <h2>주문 조회하기</h2>
-            </a>
-          </Link>
-          <Link href="/video">
-            <a>
-              <h2>공연 영상</h2>
-            </a>
-          </Link>
-          <Link href="/feedback">
-            <a>
-              <h2>고객센터</h2>
-            </a>
-          </Link>
-        </div>
+        <h1>To the Big Dipper.</h1>
+        <Link href="/product">
+          <a>
+            <h2>한빛마켓</h2>
+          </a>
+        </Link>
+        <Link href="/vieworder">
+          <a>
+            <h2>주문 조회하기</h2>
+          </a>
+        </Link>
+        <Link href="/video">
+          <a>
+            <h2>공연 영상</h2>
+          </a>
+        </Link>
+        <Link href="/feedback">
+          <a>
+            <h2>고객센터</h2>
+          </a>
+        </Link>
       </div>
-      <br />
-      <br />
-      <br />
+      asdf
     </>
   );
 };

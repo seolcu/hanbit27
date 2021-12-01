cde986fa5b70dbe500aa4563b87c18076b3f8c96
seolcu <seolcu0112@proton.me>
2021-12-01 12:00:00 +0900

    To the Big Dipper, Gothic A1

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />

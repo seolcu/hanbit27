6e2167e03c4656b60b00da22fd22aa08c6c1408f
seolcu <seolcu0112@proton.me>
2021-12-01 12:00:00 +0900

    투표 기능 만들었으나 아직 쿠키 path 설정 안됨

--- pages/maskKing/[id].js
+++ pages/maskKing/[id].js
@@ -23,25 +23,23 @@ export const getStaticProps = async ({ params }) => {
 };
 
 const MaskKingSpecificPage = ({ id }) => {
+  // 좋아요 쿠키설정
   let likeCookieName = `like${id}`;
   const maskKingInfo = maskKingInfoList[parseInt(id) - 1];
   const [likeState, setLikeState] = useState(Cookies.get(likeCookieName));
-
   useEffect(() => {
     Cookies.set(likeCookieName, likeState);
   });
 
+  // 좋아요 Firestore 설정
   const likeDoc = doc(firestore, "LikeCount", id);
   const [likeCount, setLikeCount] = useState(0);
-
   const getLike = async () => {
     const data = await getDoc(likeDoc).then(
       (snapshot) => snapshot.data().count,
     );
-    setLikeState(Cookies.get(likeCookieName));
     setLikeCount(data);
   };
-
   const updateLike = async (count) => {
     await setDoc(likeDoc, {
       count: count,
@@ -49,8 +47,24 @@ const MaskKingSpecificPage = ({ id }) => {
     getLike();
   };
 
+  // 투표 쿠키설정
+  const voteCookieName = "voted";
+  const [voteCount, setVoteCount] = useState(0);
+  const [voteState, setVoteState] = useState(Cookies.get(voteCookieName));
+
+  // 투표 Firestore 설정
+  const voteDoc = doc(firestore, "MaskKingVote", id);
+  const getVoteCount = async () => {
+    const data = await getDoc(voteDoc).then(
+      (snapshot) => snapshot.data().count,
+    );
+    setVoteCount(data);
+  };
+
+  // Firestore 데이터 불러오기
   useEffect(() => {
     getLike();
+    getVoteCount();
   });
 
   return (
@@ -64,46 +78,66 @@ const MaskKingSpecificPage = ({ id }) => {
       </Head>
       <HeaderComponent />
       <div className="container p-5">
-        <div className={`row`}>
-          <div className="col-lg-12 p-0">
-            <video controls className="w-100 p-0">
-              <source src={maskKingInfo.videoSrc} type="video/mp4" />
-            </video>
-            <div className="">
-              <h1 className="fw-bold mt-3">
-                #{maskKingInfo.num} {maskKingInfo.name}
-              </h1>
-              <div className="d-flex justify-content-between">
-                <h2>
-                  <MdMusicNote />
-                  {maskKingInfo.music} - {maskKingInfo.artist}
-                </h2>
-                <h3>
-                  {likeState == 1 ? (
-                    <MdThumbUpAlt
-                      // key 랜덤으로 강제 리덴더링...ㄷㄷ 스택오버플로우는 미쳤어... 몇시간동안 고생했는데...
-                      key={Math.random()}
-                      className="display-4 d-inline"
-                      onClick={() => {
-                        updateLike(likeCount - 1);
-                        setLikeState(0);
-                      }}
-                    />
-                  ) : (
-                    <MdThumbUpOffAlt
-                      key={Math.random()}
-                      className="display-4 d-inline"
-                      onClick={() => {
-                        updateLike(likeCount + 1);
-                        setLikeState(1);
-                      }}
-                    />
-                  )}{" "}
-                  {likeCount}
-                </h3>
-              </div>
+        <div className="p-0">
+          <video controls className="w-100 p-0">
+            <source src={maskKingInfo.videoSrc} type="video/mp4" />
+          </video>
+          <div className="">
+            <h1 className="fw-bold mt-3">
+              #{maskKingInfo.num} {maskKingInfo.name}
+            </h1>
+            <div className="d-flex justify-content-between align-items-center">
+              <h2>
+                <MdMusicNote />
+                {maskKingInfo.music} - {maskKingInfo.artist}
+              </h2>
+              <h3>
+                {likeState == 1 ? (
+                  <MdThumbUpAlt
+                    // key 랜덤으로 강제 리덴더링...ㄷㄷ 스택오버플로우는 미쳤어... 몇시간동안 고생했는데...
+                    key={Math.random()}
+                    className="display-4 d-inline"
+                    onClick={() => {
+                      updateLike(likeCount - 1);
+                      setLikeState(0);
+                    }}
+                  />
+                ) : (
+                  <MdThumbUpOffAlt
+                    key={Math.random()}
+                    className="display-4 d-inline"
+                    onClick={() => {
+                      updateLike(likeCount + 1);
+                      setLikeState(1);
+                    }}
+                  />
+                )}{" "}
+                {likeCount}
+              </h3>
             </div>
           </div>
+        </div>
+        <div className="mt-1 d-flex justify-content-center gap-2">
+          <Link href="/maskKing">
+            <a>
+              <button className="btn btn-secondary fs-3">더 둘러보기</button>
+            </a>
+          </Link>
+          <button
+            className="btn btn-primary fs-3"
+            disabled={voteState == "true" ? true : false}
+            onClick={async () => {
+              if (voteState !== "true") {
+                await setDoc(voteDoc, {
+                  count: voteCount + 1,
+                });
+              }
+              setVoteState("true");
+              Cookies.set(voteCookieName, "true");
+            }}
+          >
+            {voteState == "true" ? "투표함" : `#${maskKingInfo.num} 투표하기`}
+          </button>
         </div>
       </div>
     </>

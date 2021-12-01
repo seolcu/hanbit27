2ddd8fc8e05abcdca748d47bf1ee00794f57d51f
seolcu <seolcu0112@proton.me>
2021-12-01 12:00:00 +0900

    대규모 이름변경, 복면가왕 데이터 public, 복면가왕 세부페이지 기획

---
+++ public/data/maskKingInfoList.js
@@ -0,0 +1,100 @@
+const maskKingInfoList = [
+  {
+    num: "1",
+    name: "김상우",
+    music: "느린심장박동",
+    artist: "래원",
+    title: "김상우의 느린심장박동",
+    desc: (
+      <>
+        김상우의 기깔나는 느린심장박동!
+        <br />
+        그의 엄청난 플로우를 따라잡을 자는 누구일까?
+      </>
+    ),
+    thumbSrc: "/image/noImagePlaceHolder.png",
+    videoSrc: "/video/sample.mp4",
+  },
+  {
+    num: "2",
+    name: "김상우",
+    music: "느린심장박동",
+    artist: "래원",
+    title: "김상우의 느린심장박동",
+    desc: (
+      <>
+        김상우의 기깔나는 느린심장박동!
+        <br />
+        그의 엄청난 플로우를 따라잡을 자는 누구일까?
+      </>
+    ),
+    thumbSrc: "/image/noImagePlaceHolder.png",
+    videoSrc: "/video/sample.mp4",
+  },
+  {
+    num: "3",
+    name: "김상우",
+    music: "느린심장박동",
+    artist: "래원",
+    title: "김상우의 느린심장박동",
+    desc: (
+      <>
+        김상우의 기깔나는 느린심장박동!
+        <br />
+        그의 엄청난 플로우를 따라잡을 자는 누구일까?
+      </>
+    ),
+    thumbSrc: "/image/noImagePlaceHolder.png",
+    videoSrc: "/video/sample.mp4",
+  },
+  {
+    num: "4",
+    name: "김상우",
+    music: "느린심장박동",
+    artist: "래원",
+    title: "김상우의 느린심장박동",
+    desc: (
+      <>
+        김상우의 기깔나는 느린심장박동!
+        <br />
+        그의 엄청난 플로우를 따라잡을 자는 누구일까?
+      </>
+    ),
+    thumbSrc: "/image/noImagePlaceHolder.png",
+    videoSrc: "/video/sample.mp4",
+  },
+  {
+    num: "5",
+    name: "김상우",
+    music: "느린심장박동",
+    artist: "래원",
+    title: "김상우의 느린심장박동",
+    desc: (
+      <>
+        김상우의 기깔나는 느린심장박동!
+        <br />
+        그의 엄청난 플로우를 따라잡을 자는 누구일까?
+      </>
+    ),
+    thumbSrc: "/image/noImagePlaceHolder.png",
+    videoSrc: "/video/sample.mp4",
+  },
+  {
+    num: "6",
+    name: "김상우",
+    music: "느린심장박동",
+    artist: "래원",
+    title: "김상우의 느린심장박동",
+    desc: (
+      <>
+        김상우의 기깔나는 느린심장박동!
+        <br />
+        그의 엄청난 플로우를 따라잡을 자는 누구일까?
+      </>
+    ),
+    thumbSrc: "/image/noImagePlaceHolder.png",
+    videoSrc: "/video/sample.mp4",
+  },
+];
+
+export default maskKingInfoList;

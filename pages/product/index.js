46219453472e9b5849f2dcc8a09792dded0ed7f9
seolcu <seolcu0112@proton.me>
2021-12-01 12:00:00 +0900

    대파마켓 흑백디자인 차용, 이미지 삽입

--- pages/product/index.js
+++ pages/product/index.js
@@ -39,45 +39,36 @@ const ProductList = ({ productList }) => {
           제고의 중고장터, 대파마켓
           <br />& 제 27회 한빛제만의 아름다운 굿즈들
         </h2>
+        <div className={styles.category}>
+          <button type="button" className="btn btn-light">
+            모두
+          </button>
+          <button type="button" className="btn btn-light">
+            의류
+          </button>
+          <button type="button" className="btn btn-light">
+            교재
+          </button>
+          <button type="button" className="btn btn-light">
+            굿즈
+          </button>
+        </div>
       </div>
       <div className={styles.gridContainer}>
         {productList.map((product, index) => {
           return (
             <div className={styles.productCard} key={index}>
+              <Image
+                src={
+                  "https://firebasestorage.googleapis.com/v0/b/hanbit27-b2a04.appspot.com/o/%EC%8A%A4%ED%81%AC%EB%A6%B0.png?alt=media&token=56e0f6ac-0112-45d4-8cc1-51f4d96780d0"
+                }
+                alt="상품사진"
+                width={400}
+                height={400}
+                layout="responsive"
+              />
               <h2>{product.이름}</h2>
-              <div>{product.가격}원</div>
-            </div>
-          );
-        })}
-        {productList.map((product, index) => {
-          return (
-            <div className={styles.productCard} key={index}>
-              <h2>{product.이름}</h2>
-              <div>{product.가격}원</div>
-            </div>
-          );
-        })}
-        {productList.map((product, index) => {
-          return (
-            <div className={styles.productCard} key={index}>
-              <h2>{product.이름}</h2>
-              <div>{product.가격}원</div>
-            </div>
-          );
-        })}
-        {productList.map((product, index) => {
-          return (
-            <div className={styles.productCard} key={index}>
-              <h2>{product.이름}</h2>
-              <div>{product.가격}원</div>
-            </div>
-          );
-        })}
-        {productList.map((product, index) => {
-          return (
-            <div className={styles.productCard} key={index}>
-              <h2>{product.이름}</h2>
-              <div>{product.가격}원</div>
+              <h3>{product.가격}원</h3>
             </div>
           );
         })}

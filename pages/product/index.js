5b2678b16dc2bb91d0f24c09ee3965dcbd32f951
seolcu <gyuwon112@gmail.com>
2021-12-12 16:42:16 +0900

    관리자 로그인 모달 만듬

--- pages/product/index.js
+++ pages/product/index.js
@@ -27,6 +27,7 @@ export const getServerSideProps = async () => {
 const ProductList = ({ productList }) => {
   console.log(productList);
   const [selectedCategory, setSelectedCategory] = useState("모두");
+  const [password, setPassword] = useState("");
   const CategoryBtn = (category) => {
     return (
       <button
@@ -54,13 +55,75 @@ const ProductList = ({ productList }) => {
         <div className="container">
           <div className="d-flex gap-3 justify-content-between">
             <p className="display-1 fw-bold">한빛마켓</p>
-            <Link href="/product/upload">
-              <a>
-                <button className="mt-2 btn btn-light">
-                  상품 업로드(관리자)
-                </button>
-              </a>
-            </Link>
+            <a>
+              <button
+                type="button"
+                className="mt-2 btn btn-light"
+                data-bs-toggle="modal"
+                data-bs-target="#adminLoginModal"
+              >
+                상품 업로드(관리자)
+              </button>
+              {/* 관리자 로그인 모달창 */}
+              <div
+                className="modal fade text-dark fw-bold"
+                id="adminLoginModal"
+                tabIndex="-1"
+                aria-labelledby="adminLoginModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="adminLoginModalLabel">
+                        관리자 로그인
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      <input
+                        type="password"
+                        className="form-control form-control-lg"
+                        id="비밀번호"
+                        placeholder="비밀번호"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                      >
+                        취소
+                      </button>
+                      <Link
+                        href={
+                          password == "hanbit27auth" ? "/product/upload" : ""
+                        }
+                      >
+                        <a>
+                          <button
+                            type="button"
+                            className="btn btn-primary"
+                            data-bs-dismiss="modal"
+                            disabled={password == "hanbit27auth" ? false : true}
+                          >
+                            로그인
+                          </button>
+                        </a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
           </div>
           <h3>
             제고의 중고장터, 대파마켓

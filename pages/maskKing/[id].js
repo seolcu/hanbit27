2a88549bfff7b7ad4940bf9b76d5325ba8af9f04
seolcu <gyuwon112@gmail.com>
2021-12-17 20:09:07 +0900

    실시간 댓글창 삭제

--- pages/maskKing/[id].js
+++ pages/maskKing/[id].js
@@ -2,12 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../../styles/MaskKingSpecificPage.module.scss";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import maskKingInfoList from "../../public/data/maskKingInfoList";
 import HeaderComponent from "../../components/HeaderComponent";
-import { useEffect, useState } from "react";
-import database from "../../firebase/databaseInit";
-import { ref, set, onValue, serverTimestamp, get } from "firebase/database";
+import firestore from "../../firebase/firestoreInit";
 
 export const getStaticPaths = async () => {
   const paths = maskKingInfoList.map((maskKingInfo) => ({
@@ -23,47 +20,6 @@ export const getStaticProps = async ({ params }) => {
 
 const MaskKingSpecificPage = ({ id }) => {
   const maskKingInfo = maskKingInfoList[parseInt(id) - 1];
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
-
-  const chatRef = ref(database, "chat");
-
-  // 초기 값 불러오기
-  get(chatRef)
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        setMessages(snapshot.val());
-      } else {
-        console.log("No data available");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
-  // 값 동기화
-  onValue(chatRef, (snapshot) => {
-    if (snapshot.exists()) {
-      if (snapshot.val() !== null) {
-        setMessages(snapshot.val());
-      }
-    }
-  });
-
-  // 업로드 시
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    const trimmedMessage = newMessage.trim();
-    if (trimmedMessage) {
-      let messagesSampleList = messages;
-      messagesSampleList.push({
-        text: trimmedMessage,
-        createdAt: serverTimestamp(),
-      });
-      set(chatRef, messagesSampleList);
-      setNewMessage("");
-    }
-  };
 
   return (
     <>
@@ -77,39 +33,13 @@ const MaskKingSpecificPage = ({ id }) => {
       <HeaderComponent />
       <div className="container p-5">
         <div className={`row`}>
-          <video controls className="col-lg-8 p-0">
-            <source src={maskKingInfo.videoSrc} type="video/mp4" />
-          </video>
-          <div className="col-lg-4 p-0 d-block">
-            <div className={`overflow-auto ${styles.messageContainer}`}>
-              {messages
-                ? messages.map((message, index) => (
-                    <div key={index} className="fs-5">
-                      {message.text}
-                    </div>
-                  ))
-                : ""}
-            </div>
-            <form onSubmit={handleOnSubmit} className={`${styles.messageForm}`}>
-              <input
-                type="text"
-                className="form-control"
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-                placeholder="댓글 입력"
-              />
-              <button
-                type="submit"
-                className="btn btn-primary"
-                style={{ minWidth: "70px" }}
-                disabled={!newMessage}
-              >
-                보내기
-              </button>
-            </form>
+          <div className="col-lg-10 p-0">
+            <video controls className="w-100 p-0">
+              <source src={maskKingInfo.videoSrc} type="video/mp4" />
+            </video>
           </div>
+          <div className={`col-lg-2 ${styles.recommendBar}`}>추천영상</div>
         </div>
-        <div className={`${styles.recommendBar}`}>추천영상</div>
       </div>
     </>
   );
